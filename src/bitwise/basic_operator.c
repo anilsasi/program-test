120b833177d3c7e27b1eb79bc756538923fdb7d8
Anil sasidhar Kasulanati <anilsasi88@gmail.com>
2017-11-12 20:57:10 +0530

    add to 1

--- src/bitwise/basic_operator.c
+++ src/bitwise/basic_operator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+int add_one(int x);
 
 int odd_in_array(){
   int a[] = {4, 5, 4, 5, 17, 8, 8};
@@ -8,6 +9,7 @@ int odd_in_array(){
     res ^= a[i];
   }
   printf("%d\n", res);
+  return 0;
 }
 
 //check opposite signs
@@ -22,7 +24,22 @@ int check_sign(int a, int b){
 }
 
 int main(int argc, char const *argv[]) {
-  check_sign(-100, 100);
-  check_sign(-20, -20);
+  //check_sign(-100, 100);
+  //check_sign(-20, -20);
+  add_one(7);
+  return 0;
+}
+
+//add one to given number
+int add_one(int x){
+  int m = 1;
+  while (x&m) {
+    printf("x=%d and m=%d\n", x, m);
+    x=x^m;
+    m = m<<1;
+  }
+  //printf("x=%d and m=%d\n", x, m);
+  x=x^m;
+  printf("%d\n", x);
   return 0;
 }
