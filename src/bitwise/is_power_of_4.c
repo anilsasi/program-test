5a578a9efde12d271aac8f1b829579330bbdce3a
Anil sasidhar Kasulanati <anilsasi88@gmail.com>
2017-11-16 21:55:38 +0530

    added bitwise power of 4

--- src/bitwise/is_power_of_4.c
+++ src/bitwise/is_power_of_4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 bool ispower_of_4(long int x);
+bool is_bitwise_power_of_4(unsigned int x);
 
 int main(int argc, char const *argv[]) {
   long int number = 0;
@@ -11,7 +12,7 @@ int main(int argc, char const *argv[]) {
   if (argc < 2) {
     printf("at least one argument expected\n");
   }
-  if (ispower_of_4(number)){
+  if (is_bitwise_power_of_4(number)){
     printf("it is power of 4\n");
   }
   else{
@@ -31,4 +32,32 @@ bool ispower_of_4(long int number){
     number = number / 4;
   }
   return 1;
+}
+
+//check using bitwise operators
+int count_number_of_set_bits(unsigned int number)
+{
+    int count = 0;
+    while(number)
+    {
+        count = count+(number&1);
+        number = number >> 1;
+    }
+    return count;
+}
+
+bool is_bitwise_power_of_4(unsigned int number)
+{
+    int count = 0;
+    if (count_number_of_set_bits(number) == 1)
+    {
+        while (number > 1)
+        {
+            number = number >> 1;
+            count++;
+        }
+        return (count%2 == 0)?1:0;
+    }
+
+    return 0;
 }
