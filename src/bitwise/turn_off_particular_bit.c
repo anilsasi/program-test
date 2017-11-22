db40358c55839dd9c511333c08d3bb738f098e27
Anil sasidhar Kasulanati <anilsasi88@gmail.com>
2017-11-22 23:35:32 +0530

    src/

--- src/bitwise/turn_off_particular_bit.c
+++ src/bitwise/turn_off_particular_bit.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv)
+int test_01(int argc, char* argv)
 {
    int number = 15, reset = 0;
    int pos = 1;
    reset = number & ~(1 <<( pos - 1));
    printf("%d\n", reset); 
+}
+
+int main(int argc, char* argv[])
+{
+    int number = atoi(argv[1]);
+    int position = atoi(argv[2]);
 }
