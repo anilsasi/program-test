fee11b19bffc32beb5a6b2240d36cd6d3013cf29
Anil sasidhar Kasulanati <anilsasi88@gmail.com>
2017-11-26 23:10:29 +0530

    linked

--- src/linked/linked_list_problems.c
+++ src/linked/linked_list_problems.c
@@ -1,11 +1,12 @@
 #include <linked_list.h>
 
 node_t* head = NULL;
-node_t* temp = NULL;
 
 static inline
 void add_node(int data)
 {
+
+  node_t* temp = head;
   node_t* new_node = (node_t*)malloc(sizeof(node_t));
   new_node->data = data;
   new_node->next =NULL;
@@ -16,9 +17,9 @@ void add_node(int data)
   }
   else
   {
-  while (temp->next != NULL) {
-    temp = temp->next;
-  }
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
   temp->next = new_node;
   temp = new_node;
 }
@@ -38,6 +39,7 @@ static inline void delete_node(struct node* head)
 static inline
 void search_node(node_t* head, int data)
 {
+  node_t* temp = NULL;
   temp = head;
   int position = 1;
   while(temp->data != data)
@@ -49,21 +51,72 @@ void search_node(node_t* head, int data)
 }
 
 void
-delete_node_at_specific_position(node_t* head, int data)
+delete_node_at_specific_position(node_t* head, int position)
 {
+    node_t* temp = head;
+    node_t* previous = NULL;
+    int i = 0;
+    while(temp->next != NULL && i != position)
+    {
+        i++;
+        previous = temp;
+        temp = temp->next;
+    }
+    
+    previous->next = temp->next;
+    free(temp);
 
 }
+
+int display_linked_list()
+{
+    node_t* temp = head;
+    while(temp != NULL)
+    {
+        printf("%d->", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+    return 0;
+}
+
+void inser_at_particular_position(int position, int data)
+{
+    node_t* traverse = head;
+    int count = 0;
+    while(traverse->next != NULL & count != position)
+    {
+        traverse = traverse->next;
+    }
+    
+    node_t* new_node = (node_t*)malloc(sizeof(node_t));
+    new_node->data = data;
+    new_node->next = NULL;
+    if (traverse == NULL)
+    {
+        traverse = new_node;
+    }
+    else
+    {
+        new_node->next = traverse->next;
+        traverse->next = new_node;
+    }
+}
+
 int main()
 {
     int i = 0;
     for (i = 10; i < 100; i = i+10)
     {
         add_node(i);
-        printf("%d->", temp->data);
     }
+    printf("list is:", display_linked_list());
     printf("\n");
-    search_node(head, 70);
-    delete_node(head);
-    printf("%d\n", head->data);
+    //search_node(head, 70);
+    //delete_node_at_specific_position(head, 3);
+    //delete_node(head);
+    inser_at_particular_position(4, 32);
+    printf("after inserting:", display_linked_list());
+    printf("\n");
     return 0;
 }
